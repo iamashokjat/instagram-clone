aae0fef2c7f2356060f095cf0579a5755566eaf2
iamashokjat <ashokjakad@gmail.com>
2020-08-04 22:21:12 +0530

    fixed issues

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -39,7 +39,7 @@ const CretePost = ()=>{
    const postDetails = ()=>{
        const data = new FormData()
        data.append("file",image)
-       data.append("upload_preset","new-insta")
+       data.append("upload_preset","instagram-clone")
        data.append("cloud_name","ashokjat9671")
        fetch("https://api.cloudinary.com/v1_1/ashokjat9671/image/upload",{ 
         
